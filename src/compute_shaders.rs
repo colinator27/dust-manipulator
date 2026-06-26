d858f21446d4a27ec4de09c427d4e67dfbd09268
colinator27 <17358554+colinator27@users.noreply.github.com>
2026-06-26 11:46:22 -0400

    Attempt using fewer D3D12 resource slots

--- src/compute_shaders.rs
+++ src/compute_shaders.rs
@@ -1,7 +1,7 @@
 use std::{ffi::{c_void, CStr}, fs::{self}, ptr};
 
 use sdl3::sys::gpu::{SDL_AcquireGPUCommandBuffer, SDL_BeginGPUComputePass, SDL_BeginGPUCopyPass, SDL_BindGPUComputePipeline, SDL_BindGPUComputeStorageBuffers, SDL_CreateGPUBuffer, SDL_CreateGPUComputePipeline, SDL_CreateGPUDevice, SDL_CreateGPUTransferBuffer, SDL_DestroyGPUDevice, SDL_DispatchGPUCompute, SDL_DownloadFromGPUBuffer, SDL_EndGPUComputePass, SDL_EndGPUCopyPass, SDL_GPUBuffer, SDL_GPUBufferCreateInfo, SDL_GPUBufferRegion, SDL_GPUCommandBuffer, SDL_GPUComputePipeline, SDL_GPUComputePipelineCreateInfo, SDL_GPUCopyPass, SDL_GPUDevice, SDL_GPUFence, SDL_GPUShaderFormat, SDL_GPUStorageBufferReadWriteBinding, SDL_GPUStorageTextureReadWriteBinding, SDL_GPUTransferBuffer, SDL_GPUTransferBufferCreateInfo, SDL_GPUTransferBufferLocation, SDL_GetGPUShaderFormats, SDL_MapGPUTransferBuffer, SDL_PushGPUComputeUniformData, SDL_ReleaseGPUBuffer, SDL_ReleaseGPUComputePipeline, SDL_ReleaseGPUFence, SDL_ReleaseGPUTransferBuffer, SDL_SubmitGPUCommandBufferAndAcquireFence, SDL_UnmapGPUTransferBuffer, SDL_UploadToGPUBuffer, SDL_WaitForGPUFences, SDL_GPU_BUFFERUSAGE_COMPUTE_STORAGE_READ, SDL_GPU_BUFFERUSAGE_COMPUTE_STORAGE_WRITE, SDL_GPU_SHADERFORMAT_DXIL, SDL_GPU_SHADERFORMAT_MSL, SDL_GPU_SHADERFORMAT_SPIRV, SDL_GPU_TRANSFERBUFFERUSAGE_DOWNLOAD, SDL_GPU_TRANSFERBUFFERUSAGE_UPLOAD};
-use sdl3_sys::gpu::SDL_GPUBufferUsageFlags;
+use sdl3_sys::{gpu::{SDL_CreateGPUDeviceWithProperties, SDL_GPUBufferUsageFlags, SDL_PROP_GPU_DEVICE_CREATE_D3D12_ALLOW_FEWER_RESOURCE_SLOTS_BOOLEAN, SDL_PROP_GPU_DEVICE_CREATE_DEBUGMODE_BOOLEAN, SDL_PROP_GPU_DEVICE_CREATE_SHADERS_DXIL_BOOLEAN, SDL_PROP_GPU_DEVICE_CREATE_SHADERS_MSL_BOOLEAN, SDL_PROP_GPU_DEVICE_CREATE_SHADERS_SPIRV_BOOLEAN}, properties::{SDL_CreateProperties, SDL_DestroyProperties, SDL_SetBooleanProperty}};
 
 use crate::util;
 
@@ -31,7 +31,15 @@ impl Default for ComputePipelineInfo<'_> {
 
 pub fn create_gpu_device() -> Result<*mut SDL_GPUDevice, &'static str> {
     let device = unsafe { 
-        SDL_CreateGPUDevice(SDL_GPU_SHADERFORMAT_SPIRV | SDL_GPU_SHADERFORMAT_DXIL | SDL_GPU_SHADERFORMAT_MSL, false, std::ptr::null()) 
+        let props = SDL_CreateProperties();
+        SDL_SetBooleanProperty(props, SDL_PROP_GPU_DEVICE_CREATE_SHADERS_SPIRV_BOOLEAN, true);
+        SDL_SetBooleanProperty(props, SDL_PROP_GPU_DEVICE_CREATE_SHADERS_DXIL_BOOLEAN, true);
+        SDL_SetBooleanProperty(props, SDL_PROP_GPU_DEVICE_CREATE_SHADERS_MSL_BOOLEAN, true);
+        SDL_SetBooleanProperty(props, SDL_PROP_GPU_DEVICE_CREATE_DEBUGMODE_BOOLEAN, false);
+        SDL_SetBooleanProperty(props, SDL_PROP_GPU_DEVICE_CREATE_D3D12_ALLOW_FEWER_RESOURCE_SLOTS_BOOLEAN, true);
+        let device = SDL_CreateGPUDeviceWithProperties(props);
+        SDL_DestroyProperties(props);
+        device
     };
     if device.is_null() {
         Err("Failed to create GPU device")
